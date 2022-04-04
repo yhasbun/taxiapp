ddc6add9524ddd83230f4840af34be94344f1081
Fjborrero178 <101543070+Fjborrero178@users.noreply.github.com>
2022-04-03 23:03:39 -0500

    Update map.js

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -11,7 +11,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 async function getGPS() {
-    response = await fetch("http://taxilocationafb.ddns.net//gps");
+    response = await fetch("http://taxilocationafb.ddns.net/gps");
     coordinates = await response.json();
     console.log(coordinates);
     document.getElementById("LatID").textContent = coordinates.lat;
