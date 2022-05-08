33a33c0572d8ea4ec43ab07d7f7066ad898e3e91
Fjborrero178 <101543070+Fjborrero178@users.noreply.github.com>
2022-05-08 10:23:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ server.listen(PORT, function () {
         date = msg_values[2].split(',')[0].trim()
         time = msg_values[2].split(',')[1].trim()
         rpm = msg_values[3].split(',')
-        rpm = msg_values[4].split(',')[1].trim()
+        carro = msg_values[4].split(',')
         
         coords = {
             lat: lat,
