6b936cfb438cc1024a76d23738273ccf72759e76
Fjborrero178 <101543070+Fjborrero178@users.noreply.github.com>
2022-05-08 10:13:22 -0500

    Update map.js

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,10 +1,9 @@
 var map = L.map('MapID').setView({ lat: 11.008, lng: -74.809 });
 map.setZoom(15);
-var PORT_RES = process.env.RES;
 
 // Icon options
 var iconOptions = {
-  iconUrl: "logo1.png",
+  iconUrl: 'logo1.png',
   iconSize: [50, 50]
 }
 // Creating a custom icon
@@ -14,11 +13,11 @@ var customIcon = L.icon(iconOptions);
 var markerOptions = {
   title: "MyLocation",
   clickable: true,
-  draggable: false,
+  draggable: true,
   icon: customIcon
 }
 // Creating a Marker
-let marker = L.marker({ lat: 11.008, lng: -74.809 }, markerOptions).addTo(map);
+let marker = L.marker({ lat: 11.008, lng: -74.809 }, markerOptions);
 /* let marker = L.marker({ lat: 11.008, lng: -74.809 },{ title: "Me" },{color:"#cf0404"}).addTo(map); */
 let polyline = L.polyline([], {color: '#41b611', smoothFactor:3}).addTo(map);
 const coords_records = [];
